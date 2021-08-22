5d66e89cf950709b0c8a0edfa2640a4614eeb762
wpbindt <wesselbindt@gmail.com>
2021-08-23 00:13:11 +0200

    Make tests pass

--- src.py
+++ src.py
@@ -12,7 +12,14 @@ class NestedObject(pydantic.BaseModel):
     things: list[Thing]
 
 
+def _prepare_for_hashing(nested_object: NestedObject) -> NestedObject:
+    nested_object_copy = nested_object.copy(deep=True)
+    nested_object_copy.things = sorted(nested_object_copy.things, key=lambda thing: thing.content)
+    return nested_object_copy
+
+
 def hash_function(nested_object: NestedObject) -> str:
     m = hashlib.md5()
-    m.update(bytes(nested_object.json(), 'utf-8'))
+    hashable = _prepare_for_hashing(nested_object)
+    m.update(bytes(hashable.json(), 'utf-8'))
     return m.hexdigest()
